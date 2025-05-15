58a9545327f4f053fb28fa07ddfd6a4a696a7577
RONO <113999110+Titusrono@users.noreply.github.com>
2025-05-15 08:59:30 +0300

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FarmerComponent } from './pages/farmer/farmer.component';
 import { VendorComponent } from './pages/vendor/vendor.component';
-import { ProducelistComponent } from './pages/produce-list/produce-list.component';
 import { PaymentComponent } from './pages/payment/payment.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { ChatComponent } from './pages/chat/chat.component';
+import { ProduceComponent } from './pages/produce/produce.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 export const routes: Routes = [
     {path: '','title':'Home','component': HomeComponent},
-    {path: 'farmer',component: FarmerComponent},
-    {path: 'vendor',component: VendorComponent},
-    {path: 'producelist',component: ProducelistComponent},
-    {path: 'payment',component: PaymentComponent},
-    {path: 'orders',component: OrdersComponent},
-    {path: 'chat',component: ChatComponent}
+    {path: 'farmer','title':'Farmer',component: FarmerComponent},
+    {path: 'vendor','title':'Vendor',component: VendorComponent},
+    {path: 'produce','title':'Produce',component: ProduceComponent},
+    {path: 'payment','title':'Payment',component: PaymentComponent},
+    {path: 'orders','title':'Orders',component: OrdersComponent},
+    {path: 'chat','title':'Chat',component: ChatComponent},
+    {path:'register','title':'Register',component: RegisterComponent},
+    {path:'**','title':'400',component: ErrorComponent}
 ];
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  imports: [CommonModule,RouterLink],
+  standalone: true
 })
 export class HeaderComponent {
   menuOpen = false;
