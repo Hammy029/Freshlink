41a929bc7e13be24e47a7732595df873da7f57a4
RONO <113999110+Titusrono@users.noreply.github.com>
2025-06-17 17:01:48 +0300

    init

--- src/app/dashboard/usercategory/usercategory.component.ts
+++ src/app/dashboard/usercategory/usercategory.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Category, UsercategoryService } from '../../../services/usercategory.service';
+import { Category, UsercategoryService } from '../../services/usercategory.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
